601968c434c2d4f2f683cc2033d6f31a961ede8e
giusfds <giuseppe.cordeiro@gmail.com>
2023-06-12 14:57:50 -0300

    first commit

--- Maratona/Simulado/idade/idade de dona monica.c
+++ Maratona/Simulado/idade/idade de dona monica.c
@@ -2,7 +2,18 @@
 
 int main() {
 
+    int monica,f1,f2,f3;
+
+    scanf("%d", &monica);
+    scanf("%d", &f1);
+    scanf("%d", &f2);
+
+    f3 = monica - (f1+f2);
+
     
+    
+
+
 
     printf("\n--------- | FIM DO PROGRAMA | ---------\n\n");
     return 0;
