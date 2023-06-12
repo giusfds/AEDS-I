806a3036fde7f9ff1e7e2d79d3ffd79c291860b4
giuseppe cordeiro <giuseppe.cordeiro@gmail.com>
2023-06-12 17:27:19 -0300

    first commit

--- Maratona/Simulado/idade/idade de dona monica.c
+++ Maratona/Simulado/idade/idade de dona monica.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    int monica,f1,f2,f3;
+    int monica=0,f1=0,f2=0,f3=0, maisvelho=0;
 
     scanf("%d", &monica);
     scanf("%d", &f1);
@@ -10,10 +10,27 @@ int main() {
 
     f3 = monica - (f1+f2);
 
-    
+    if (f1>f2)
+    {
+        if (f1 > f3)
+        {
+            maisvelho = f1;
+        }else{
+            maisvelho = f3;
+        }
+        
+    }else{
+        if (f2 > f3)
+        {
+            maisvelho = f2;
+        }else{
+            maisvelho = f3; 
+        }
+        
+    }
     
 
-
+    printf("%d\n", maisvelho);
 
     printf("\n--------- | FIM DO PROGRAMA | ---------\n\n");
     return 0;
