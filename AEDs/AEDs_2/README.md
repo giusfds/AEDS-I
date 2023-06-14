d171e5c5288cb6c728fe8b4905a0a6ed3c60793f
giusfds <giuseppe.cordeiro@gmail.com>
2023-06-14 15:32:45 -0300

    .

---
+++ AEDs/AEDs_2/README.md
@@ -0,0 +1,3 @@
+#  Algoritmos e Estruturas de Dados II || (AEDS2)
+
+>aqui esta meus arquivos de estudo, onde tem tudo o que estamos aprendendo na materia!  
---
+++ AEDs/AEDs_2/soma.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class soma {
+	public static Scanner sc = new Scanner(System.in);
+
+	public static void main(String args[]) {
+
+		int num1, num2, soma;
+
+		System.out.println("Digite um numero");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro numero");
+		num2 = sc.nextInt();
+
+		soma = num1 + num2;
+
+		System.out.println("Soma:" + soma);
+
+	}
+
+	
+
+}
