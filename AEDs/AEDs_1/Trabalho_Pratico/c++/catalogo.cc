8ea61625e9cfe1c3fc52237252d37d11647862f7
giusfds <giuseppe.cordeiro@gmail.com>
2023-06-20 17:43:28 -0300

    .

--- AEDs/AEDs_1/Trabalho_Pratico/c++/catalogo.cc
+++ AEDs/AEDs_1/Trabalho_Pratico/c++/catalogo.cc
@@ -39,20 +39,19 @@ public:
   int getAno() { return ano; }
   string getPlaca() { return placa; }
 
-
   // Métodos set
 
   void setModelo(string novoModelo) { modelo = novoModelo; }
   void setMarca(string novaMarca) { marca = novaMarca; }
-  void setTipo(){
+  void setTipo() {
     string tipo;
     do {
       tipo = readString();
-    } while (tipo != "carro" || tipo != "Carro" || 
-             tipo != "moto"  || tipo != "Moto"
-    );
-    
-    this->tipo = tipo;} 
+    } while (tipo != "carro" || tipo != "Carro" || tipo != "moto" ||
+             tipo != "Moto");
+
+    this->tipo = tipo;
+  }
   void setObs(string novaObs) { obs = novaObs; }
   void setPlaca(string placa) { this->placa = placa; }
   void setPreco(string preco) {
@@ -60,14 +59,12 @@ public:
   }
   void setAno() {
 
-    while(true){
-     try
-     {
-      this->ano = stoi(readString());
-      break;
-     }
-     catch(...) { }
-     
+    while (true) {
+      try {
+        this->ano = stoi(readString());
+        break;
+      } catch (...) {
+      }
     }
   }
 
@@ -80,7 +77,7 @@ int menu() {
   do {
     cout << "Ola, aqui em baixo esta nosso menu, escolha a opcao" << endl;
     cout << "[1] - para listar todos os veiculos" << endl;
-    cout << "[2] - para pesquisar os veiculos" << endl;
+    cout << "[2] - para pesquisar os veiculos" << endl; // feito
     cout << "[3] - para cadastrar um novo veiculo" << endl; // feito
     cout << "[4] - para editar o cadastro do veiculo" << endl;
     cout << "[5] - excluir um carro ja existente" << endl;
@@ -96,43 +93,40 @@ void cadastro(veiculo VetVeiculos[]) {
   fstream arquivo("veiculos.txt", ios::app);
 
   cout << "qual e a placa do veiculo" << endl; // com a placa na classe provada
-  VetVeiculos[0 /*a definir*/].setPlaca(readString());
+  VetVeiculos[veiculo::contador].setPlaca(readString());
   cout << "qual e o ano do veiculo" << endl; // ano
-  VetVeiculos[0 /*a definir*/].setAno();
+  VetVeiculos[veiculo::contador].setAno();
   cout << "qual e o preco do veiculo" << endl; // preco
-  VetVeiculos[0 /*a definir*/].setPreco(readString());
+  VetVeiculos[veiculo::contador].setPreco(readString());
   cout << "qual e o modelo do veiculo" << endl; // modelo
-  VetVeiculos[0 /*a definir*/].setModelo(readString());
+  VetVeiculos[veiculo::contador].setModelo(readString());
   cout << "qual e a marca do veiculo" << endl; // marca
-  VetVeiculos[0 /*a definir*/].setMarca(readString());
+  VetVeiculos[veiculo::contador].setMarca(readString());
   cout << "qual e o tipo de veiculo (carro ou moto)" << endl; // tipo
-  VetVeiculos[0 /*a definir*/].setTipo();
+  VetVeiculos[veiculo::contador].setTipo();
   cout << "tem alguma obs" << endl; // obs
-  VetVeiculos[0 /*a definir*/].setObs(readString());
-  
-    
-  //printar no aquivo
+  VetVeiculos[veiculo::contador].setObs(readString());
 
-  arquivo << VetVeiculos[0 /*a definir*/].getPlaca();
-  arquivo << VetVeiculos[0 /*a definir*/].getAno();
-  arquivo << VetVeiculos[0 /*a definir*/].getPreco();
-  arquivo << VetVeiculos[0 /*a definir*/].getModelo();
-  arquivo << VetVeiculos[0 /*a definir*/].getMarca();
-  arquivo << VetVeiculos[0 /*a definir*/].getTipo();
-  arquivo << VetVeiculos[0 /*a definir*/].getObs();
-  
-  //arrumar a parte de printar no arquivo 
+  // printar no aquivo
+
+  arquivo << VetVeiculos[veiculo::contador].getPlaca();
+  arquivo << VetVeiculos[veiculo::contador].getAno();
+  arquivo << VetVeiculos[veiculo::contador].getPreco();
+  arquivo << VetVeiculos[veiculo::contador].getModelo();
+  arquivo << VetVeiculos[veiculo::contador].getMarca();
+  arquivo << VetVeiculos[veiculo::contador].getTipo();
+  arquivo << VetVeiculos[veiculo::contador].getObs();
+
+  // arrumar a parte de printar no arquivo
 
   cout << "O carro foi cadastrado!" << endl;
 }
 
-veiculo pesquisa(veiculo VetVeiculos[], string placa){
-  
-  for (int i = 0; i < 5; i++)
-  {
-    if (VetVeiculos[i].getPlaca() == placa)
-    {
-      return VetVeiculos[i];  
+veiculo pesquisa(veiculo VetVeiculos[], string placa) {
+
+  for (int i = 0; i < 5; i++) {
+    if (VetVeiculos[i].getPlaca() == placa) {
+      return VetVeiculos[i];
     }
   }
 
@@ -140,8 +134,6 @@ veiculo pesquisa(veiculo VetVeiculos[], string placa){
 
   // return VetVeiculos[0];
 }
-
-
 
 int main() {
 
@@ -152,13 +144,13 @@ int main() {
     switch (Menu) {
     case 1:
       /* parte para listar todos os veiculos */
-      
+
       break;
     case 2:
       /* pesquisar todos os veiculos */
       try {
         pesquisa(VetVeiculos, readString());
-      } catch(exception erro) {
+      } catch (exception erro) {
         cout << erro.what() << endl;
       }
       break;
@@ -183,10 +175,11 @@ int main() {
 }
 /*
 1) como faz o ngs de edicao e e deletar
-2) qnd deleta, por estar colocando em um verto, posso deixar um "buraco" no vetor,
-ou eu tenho que lidar com o suposto "buraco"
-3) sujestao de como primtar as linhas do arquivode forma mais facil (sem se usando while(file.eof())
-{file >> print})
+
+2) qnd deleta, por estar colocando em um verto, posso deixar um "buraco" no
+vetor, ou eu tenho que lidar com o suposto "buraco" 3) sujestao de como primtar
+as linhas do arquivode forma mais facil 
+(sem se usando while(file.eof()) {file>> print})
 
 Respostas:
 1)
