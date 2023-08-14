fca7f3c07958720e81d55eac9835e52808928eb8
giusfds <giuseppe.cordeiro@gmail.com>
2023-08-14 18:15:38 -0300

    start my library

--- aeds_theldo/bibliotecas/Giuseppe.h
+++ aeds_theldo/bibliotecas/Giuseppe.h
@@ -3,7 +3,7 @@
 // All library rights reserved for Giuseppe Sena Coridero,
 // PUC Minas student.
 
-//bibliotecas
+//libraries
 #include <stdio.h>     // para entradas e saidas
 #include <stddef.h>    // para definicoes basicas
 #include <stdlib.h>    // para a biblioteca padrao
@@ -17,11 +17,28 @@
 #include <iso646.h>    // para and, or, xor, not alternatives
 #include <locale.h>    // para aceitar teclados padroes PT-BR
 
-// variaveis globais
+// global variables
 bool trace   = true;             // inicialmente habilitado
+#define PI 3.14159265358979323846
 
-// redefinicoes
+// resets
 #define  print         printf    // melhor: print
+#define  scan          scanf     // melhor: scanf
+
+//define end or e
+#define AND &&
+#define OR ||
+#define NOT !
+
+//some colors to terminals
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define YELLOW "\x1b[33m"
+#define BLUE "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN "\x1b[36m"
+#define RESET "\x1b[0m"
+#define BOLD "\033[1m"
 
 
 //Function to check if the number is bigger than 0
@@ -72,6 +89,19 @@ void bubbleSort (int vetor[], int n) {
     }
 } //end
 
+// Selection Sort para Inteiros em ordem Crescente
+void SelectionSort(int* array, int arrayLength) {
+	for (int i = 0; i < arrayLength - 1; i++) {
+		int menor = i;
+		for (int j = i + 1; j < arrayLength; j++) {
+			if (array[menor] > array[j]) menor = j;
+		}
+		int swap	 = array[menor];
+		array[menor] = array[i];
+		array[i]	 = swap;
+	}
+}// end
+
 //Function to print the arrays
 void printArray(int arr[], int size)
 {
@@ -93,3 +123,28 @@ void pause ( const char * const text )
     } 
 } //end
 
+//Function only to identify
+void id ( const char * const text )
+{
+    printf ( "%s\n", text );
+    printf ( "%s\n", "Autor: Giuseppe Sena Cordeiro - 801779" );
+    printf ( "\n" );          
+} //end
+
+// Function to clear terminal
+void clear() {
+	system("cls");
+} // end
+
+//toUper and toLower
+void toUpper(char* string) {
+	for (int i = 0; i < strlen(string); i++) {
+		if (string[i] >= 'a' AND string[i] <= 'z') {
+			string[i] -= 32;
+		}
+	}
+}
+char toLower(char c) {
+	if (c >= 'A' AND c <= 'Z') return c += 32;
+	return c;
+} //end
