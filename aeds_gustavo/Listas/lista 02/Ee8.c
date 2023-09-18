1b8cb30a78d3e35de13b3bb7457d3b99f5cb234c
giusfds <giuseppe.cordeiro@gmail.com>
2023-09-18 19:21:05 -0300

    .

--- aeds_gustavo/Listas/lista 02/Ee8.c
+++ aeds_gustavo/Listas/lista 02/Ee8.c
@@ -3,7 +3,7 @@
 double calcPorcentage(double salario, double porcentagem){
     double x=0.0;
 
-    x = (salario * porcentagem)/100;
+    x = (salario * (porcentagem/100));
 
     return x;
 }
@@ -28,16 +28,16 @@ int main() {
     if (salario <= 2112.00)
     {
         irpf = calcPorcentage(salario, 0);
-    } else if((salario <= 2112.01) OR (salario >= 2826.65))
+    } else if((salario >= 2112.01) OR (salario <= 2826.65))
     {
         irpf = calcPorcentage(salario, 7.5);
-    } else if((salario <= 2826.66) OR (salario >= 3751.05))
+    } else if((salario >= 2826.66) OR (salario <= 3751.05))
     {
         irpf = calcPorcentage(salario, 15);
-    } else if((salario <= 3751.06) OR (salario >= 4664.68))
+    } else if((salario >= 3751.06) OR (salario <= 4664.68))
     {
         irpf = calcPorcentage(salario, 22.5);
-    } else if (salario <= 4664.68)
+    } else if (salario >= 4664.68)
     {
         irpf = calcPorcentage(salario, 27.5);
     }
@@ -46,22 +46,22 @@ int main() {
     if (salario <= 1320.00)
     {
         inss = calcPorcentage(salario, 7.5);
-    }else if ((salario <= 1320.01 ) OR (salario >= 2571.29))
+    }else if ((salario >= 1320.01 ) OR (salario <= 2571.29))
     {
         inss = calcPorcentage(salario, 9);
-    }else if ((salario <= 2571.30 ) OR (salario >= 3856.94))
+    }else if ((salario >= 2571.30 ) OR (salario <= 3856.94))
     {
         inss = calcPorcentage(salario, 12);
-    }else if ((salario <= 3856.95 ) OR (salario >= 7507.49))
+    }else if ((salario >= 3856.95 ) OR (salario <= 7507.49))
     {
         inss = calcPorcentage(salario, 14);
-    }else if (salario <= 7507.49)
+    }else if (salario >= 7507.49)
     {
         inss = calcINSS(); //funcao diferente para calcular a % especifica
     }
     
-    printf("IRPF:\n", irpf);
-    printf("INSS:\n", inss);
+    printf("IRPF: %.2lf\n", irpf);
+    printf("INSS: %.2lf\n", inss);
 
     pause ( "Apertar ENTER para terminar" );
     clear();
