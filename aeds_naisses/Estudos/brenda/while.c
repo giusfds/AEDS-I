2c2212dabb22e818485288fa033dde0766636780
giusfds <giuseppe.cordeiro@gmail.com>
2023-09-26 17:40:23 -0300

    .

---
+++ aeds_naisses/Estudos/brenda/while.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    // 0 felso
+    // 1 verdadeiro
+
+    int x;
+
+    if (1)
+    {
+        printf("oi\n");
+    }
+    
+    
+    
+
+    printf("\n\n--------- | FIM DO PROGRAMA | ---------\n\n");
+    return 0;
+}
